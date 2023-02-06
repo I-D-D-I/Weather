4720aace94bbdb0695103eb1019c9139bedc10c7
I-D-D-I <3711080@gmail.com>
2023-02-06 21:02:47 +0500

    fix scroll

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,21 @@
 const btn = document.querySelector('.header__btn');
 const page = document.querySelector('header__card')
 const module = document.querySelector('.module__wrapper');
+const body = document.querySelector('body');
 // const card = document.querySelector('.module__card');
 
 function getOpen() {
   module.style.display = 'block';
+  body.style.height = '100vh';
+  body.style.overflow = 'hidden';
 }
 btn.addEventListener('click', getOpen)
 
 function getClose(e) {
   if (e.target == module) {
     module.style.display = "none";
+    body.style.overflow = 'visible';
+    body.style.height = 'auto';
   }
 }
 window.addEventListener('click', getClose)
